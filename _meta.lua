2236bb573c7547932af4c1365ea0da0f2eec2741
antique1990 <100549769+antique1990@users.noreply.github.com>
2026-07-28 12:25:59 +0800

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,7 +0,0 @@
-return {
-    name = "Crypto Price",
-    fullname = "Cryptocurrency Price Display",
-    description = "显示 BTC、ETH、DOGE、UNI 等币种实时价格",
-    author = "Your Name",
-    version = "1.0",
-}
